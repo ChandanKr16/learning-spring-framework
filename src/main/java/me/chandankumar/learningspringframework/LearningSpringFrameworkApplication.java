23da773b344e448f8fee7b607081c4ef75319a41
ChandanKr16 <chandan.kumar2021@vitstudent.ac.in>
2023-01-27 14:04:45 +0530

    Empty project created

---
+++ src/main/java/me/chandankumar/learningspringframework/LearningSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package me.chandankumar.learningspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringFrameworkApplication.class, args);
+    }
+
+}
